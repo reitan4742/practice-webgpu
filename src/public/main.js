a50d12a43b0ef458f4b0ba2daa870c0dcfffe452
Reiya Matsumoto <ce235037@meiji.ac.jp>
2023-04-16 20:11:25 +0900

    triangleの実装

--- src/public/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-if (!navigator.gpu) {
-  console.log("WebGPU使えへんなぁ")
-} else {
-  console.log("WebGPU使えるなぁ")
-}
--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const PORT = 8080;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+app.get("/triangle", (req, res) => {
+    res.sendFile(__dirname + "/public/triangle.html");
 });
 
 app.listen(PORT);
